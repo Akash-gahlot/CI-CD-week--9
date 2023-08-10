2ae01e51ee44c052ae14c52d4bd67c201340cdb6
Akash Gahlot <akashgahlot@Akashs-MBP.bbrouter>
2023-08-10 14:58:08 +0530

    route updated

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -18,7 +18,7 @@ router.get("/me", authenticateJwt, async (req, res) => {
     })
 });
 router.post("/check", (req,resp) => { 
-  resp.send("Welcome to EC2 AWS machine");
+  resp.send("Welcome to AWS");
 })
 router.post('/signup', (req, res) => {
     const { username, password } = req.body;
