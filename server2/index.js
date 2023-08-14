cdef65b69dfa115b0942c0701454d763389f6bfc
Akash Gahlot <akashgahlot@Akashs-MBP.bbrouter>
2023-08-14 21:11:40 +0530

    small route change

--- server2/index.js
+++ server2/index.js
@@ -3,11 +3,11 @@ const app = express();
 const PORT = 3000;
 
 app.get("/name", (req, resp) => {
-    resp.send("My name is akash galot")
+    resp.send("My name is akash galot and this is my machine")
 });
 
 app.get("/EC2", (req, resp) => { 
-    resp.send("I am up over AWS EC2 ");
+    resp.send("I am up over AWS  instance of akash gahlot ");
 })
 app.listen(PORT, () => { 
     console.log("Server is up on PORT : " + PORT);    
